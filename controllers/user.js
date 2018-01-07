dbd1b8ac6bcafd692e5bfd47b11275fd39b0d27a
MuhammadOsamaKhalid <osamakhalid12@yahoo.com>
2018-01-07 22:09:02 +0500

    user file

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,4 @@
-var user = require('../models/users');
+var users = require('../models/users');
 
 var Users = {
 	getUsers:function(req, res, next) {
@@ -7,6 +7,7 @@ var Users = {
 			//On success we will do some operation 
 			//e.g we can render template here etc..
 			res.send('respond with a resource');
+		 
 		}).catch(function(error){
 			//Here we have caught error exceptions thrown by Promise
 			res.send('respond with error in DB');
