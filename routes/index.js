ba1e0a6808fc1f8c0f1bbe6ac829e77b9271e371
MuhammadOsamaKhalid <osamakhalid12@yahoo.com>
2018-01-07 22:18:22 +0500

    index file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
+var user = require('../controllers/user');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express 1' });
 });
+
+router.get('/signup', function(req, res){
+  res.render('signup.ejs');
+});
+
+
+router.get('/login', function(req, res){
+  res.render('login.ejs');
+});
+
 
 module.exports = router;
