bf00775082afa6462933fee40b87c84d925c4b01
MuhammadOsamaKhalid <osamakhalid12@yahoo.com>
2018-01-07 22:10:55 +0500

    role file

---
+++ models/Roles.js
@@ -0,0 +1,27 @@
+var DB = require('../config/db');
+/**
+* All user related methods will be define in this class
+* create, update, delete,edit users DB queries
+* DB is a object of mysql connection, where you can run all the SQL Queries
+*/
+var  Roles= {
+	getRoles:function(){
+		//Due to Async JS nature we had used Promise
+		//For detail: https://www.promisejs.org/
+		return new Promise(function (fulfill, reject){
+		   DB.query("select * from Roles",function(err, rows, fields){
+			   DB.end(); //It required to close DB connection instance 
+			   if (!err){
+				 console.log('The solution is: ', rows);// It fetch rows inside DB tables
+					fulfill(rows); //It will return all the table rows;
+			   }else{
+				 console.log('Error while performing Query.',err); //It will print all the DB errors
+				 reject(err); //It will reject request with throwing errors
+			   }
+		   });
+		});
+		
+	}
+};
+
+module.exports = Roles;
